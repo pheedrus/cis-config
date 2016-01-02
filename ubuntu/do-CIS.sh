4a371bfa62103cd29e11e2a96ed29471a5636238
Justin R. Andrusk <jandrusk@gmail.com>
2016-01-02 16:19:33 -0500

    modified:   do-CIS.sh

--- ubuntu/do-CIS.sh
+++ ubuntu/do-CIS.sh
@@ -3,10 +3,18 @@
 do-backup() {
     files=("/etc/ssh/sshd_config" "/etc/audit/audit.rules" "/etc/fstab" "/etc/login.defs" "/etc/sysctl.conf" "/etc/hosts.deny" "/etc/hosts.allow")
     backup_dir="/opt/CIS"
-    for i in "${files[@]}"
-	     do
+    if [ ! -d "$backup_dir" ]; then
+	sudo mkdir $backup_dir
+	for i in "${files[@]}"
+	do
 		 sudo cp $i $backup_dir
-    done
+	done
+    else
+	for i in "${files[@]}"
+	do
+		 sudo cp $i $backup_dir
+	done
+    fi
 }
 
 do-backup
